6bbd8d3e9c8cd9e31c129a04c3cd647bc7c1f271
S.M. SHAHi <s.m.kamalhussain@gmail.com>
2024-05-11 07:59:40 +0600

    migration process updated no need to create migration file and register the class and method while creating table when migrate command run this time file will be created and run the up method. similarly work for migrate:fresh command

--- lib/commands/migrate_command.dart
+++ lib/commands/migrate_command.dart
@@ -1,7 +1,32 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:vania_cli/models/migrate_file_model.dart';
+import 'package:vania_cli/utils/functions.dart';
+
 import 'command.dart';
+
+String migrateFileContents = '''
+import 'dart:io';
+
+import 'package:vania/vania.dart';
+
+import '../../config/database.dart';
+
+void main() async {
+  Env().load();
+  await Migrate().registry();
+  await MigrationConnection().closeConnection();
+  exit(0);
+}
+
+class Migrate {
+  registry() async {
+    await MigrationConnection().setup(databaseConfig);
+    await MigrationDatabaseTable().up();
+  }
+}
+''';
 
 class MigrateCommand implements Command {
   @override
@@ -13,9 +38,56 @@ class MigrateCommand implements Command {
   @override
   void execute(List<String> arguments) async {
     print('\x1B[32m Migration started \x1B[0m');
+
+    final files = Directory("${Directory.current.path}/lib/database/migrations")
+        .listSync();
+
+    List<MigrateFileModel> migrationFiles = getMigrationFileList(files);
+
+    String migrationFilePath =
+        "${Directory.current.path}/lib/database/migrations/.migrate.dart";
+    File migrate = File(migrationFilePath);
+
+    if (!migrate.existsSync()) {
+      migrate.createSync(recursive: true);
+    } else {
+      migrateFileContents = migrate.readAsStringSync();
+    }
+
+    final importRegExp = RegExp(r'import .+;');
+    final registryConstructorRegex =
+        RegExp(r'registry\s*\(\s*\)\s*async?\s*\{\s*([\s\S]*?)\s*\}');
+
+    if (migrationFiles.isNotEmpty) {
+      for (final fileModel in migrationFiles) {
+        // Find import statement and append new import
+        var importMatch = importRegExp.allMatches(migrateFileContents);
+        if (importMatch.isNotEmpty) {
+          migrateFileContents = migrateFileContents.replaceFirst(
+            importMatch.last.group(0).toString(),
+            "${importMatch.last.group(0)}\nimport '${fileModel.fileName}';",
+          );
+        }
+
+        // Find registry and dropTables constructors, and replace with modified versions
+        Match? registryRepositoriesBlockMatch =
+            registryConstructorRegex.firstMatch(migrateFileContents);
+
+        if (registryRepositoriesBlockMatch != null) {
+          migrateFileContents = migrateFileContents.replaceAll(
+            registryConstructorRegex,
+            '''registry() async {\n\t\t${registryRepositoriesBlockMatch.group(1)}\n\t\t await ${fileModel.name}().up();\n\t}''',
+          );
+        }
+
+        // Write modified content back to file
+        migrate.writeAsStringSync(migrateFileContents);
+      }
+    }
+
     Process process = await Process.start('dart', [
       'run',
-      '${Directory.current.path}/lib/database/migrations/migrate.dart',
+      migrationFilePath,
     ]);
 
     await for (var data in process.stdout.transform(utf8.decoder)) {
@@ -26,5 +98,8 @@ class MigrateCommand implements Command {
         }
       }
     }
+
+    ///Delete the migration file after database up
+    migrate.delete();
   }
 }
--- lib/commands/migrate_fresh_command.dart
+++ lib/commands/migrate_fresh_command.dart
@@ -1,8 +1,32 @@
 import 'dart:io';
 
-import 'package:vania_cli/commands/command_runner.dart';
+import 'package:vania_cli/models/migrate_file_model.dart';
+import 'package:vania_cli/utils/functions.dart';
 
 import 'command.dart';
+import 'command_runner.dart';
+
+String migrateFileContents = '''
+import 'dart:io';
+
+import 'package:vania/vania.dart';
+
+import '../../config/database.dart';
+
+void main() async {
+  Env().load();
+  await Migrate().dropTables();
+  await MigrationConnection().closeConnection();
+  exit(0);
+}
+
+class Migrate {
+  dropTables() async {
+    await MigrationConnection().setup(databaseConfig);
+    await MigrationDatabaseTable().down();
+  }
+}
+''';
 
 class MigrateFreshCommand implements Command {
   @override
@@ -14,15 +38,65 @@ class MigrateFreshCommand implements Command {
   @override
   void execute(List<String> arguments) async {
     print('\x1B[32m Dropping tables ........... \x1B[0m');
+
+    final files = Directory("${Directory.current.path}/lib/database/migrations")
+        .listSync();
+
+    List<MigrateFileModel> migrationFiles = getMigrationFileList(files);
+
+    String migrationFilePath =
+        "${Directory.current.path}/lib/database/migrations/.migrate.dart";
+    File migrate = File(migrationFilePath);
+
+    if (!migrate.existsSync()) {
+      migrate.createSync(recursive: true);
+    } else {
+      migrateFileContents = migrate.readAsStringSync();
+    }
+
+    final importRegExp = RegExp(r'import .+;');
+    final dropTableConstructorRegex =
+        RegExp(r'dropTables\s*\(\s*\)\s*async?\s*\{\s*([\s\S]*?)\s*\}');
+
+    if (migrationFiles.isNotEmpty) {
+      for (final fileModel in migrationFiles) {
+        // Find import statement and append new import
+        var importMatch = importRegExp.allMatches(migrateFileContents);
+        if (importMatch.isNotEmpty) {
+          migrateFileContents = migrateFileContents.replaceFirst(
+            importMatch.last.group(0).toString(),
+            "${importMatch.last.group(0)}\nimport '${fileModel.fileName}';",
+          );
+        }
+
+        // Find registry and dropTables constructors, and replace with modified versions
+        Match? dropTableRepositoriesBlockMatch =
+            dropTableConstructorRegex.firstMatch(migrateFileContents);
+
+        if (dropTableRepositoriesBlockMatch != null) {
+          migrateFileContents = migrateFileContents.replaceAll(
+            dropTableConstructorRegex,
+            '''dropTables() async {\n\t\t${dropTableRepositoriesBlockMatch.group(1)}\n\t\t await ${fileModel.name}().down();\n\t }''',
+          );
+        }
+
+        // Write modified content back to file
+        migrate.writeAsStringSync(migrateFileContents);
+      }
+    }
+
     await Process.start(
       'dart',
       [
         'run',
-        '${Directory.current.path}/lib/database/migrations/migrate.dart',
-        'migrate:fresh',
+        migrationFilePath,
       ],
     );
 
+    ///Delete the migration file after database down
+    migrate.delete();
+
+    ///Re-run the migrate command
     CommandRunner().run(["migrate"]);
   }
 }
