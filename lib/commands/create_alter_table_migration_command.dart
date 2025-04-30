c364c34af9fad89e00d442f3c6e940a51cc8e70f
Javad Zobeidi <45537050+javad-zobeidi@users.noreply.github.com>
2025-04-30 15:22:59 +0330

    Add interact cli

--- lib/commands/create_alter_table_migration_command.dart
+++ lib/commands/create_alter_table_migration_command.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:interact_cli/interact_cli.dart';
 import 'package:vania_cli/common/recase.dart';
 import 'package:vania_cli/utils/_pluralize.dart';
 import 'package:vania_cli/utils/functions.dart';
@@ -59,28 +60,55 @@ class CreateAlterTableMigrationCommand implements Command {
   String get description =>
       'Create a new alter table migration file. This command allows you to add a new column to an existing table or alter an existing column';
 
+  String _readMigrationName() {
+    final name =
+        Input.withTheme(
+          theme: Theme.defaultTheme,
+          prompt: 'What should the migration be named?',
+          defaultValue: 'interact',
+          validator: (x) {
+            if (x.contains(RegExp(r'^[a-zA-Z][a-zA-Z0-9_/\\]*$'))) {
+              throw ValidationError(
+                'Migration must contain only letters a-z, numbers 0-9 and optional _',
+              );
+            }
+            return true;
+          },
+        ).interact();
+
+    if (name.isEmpty) {
+      return _readMigrationName();
+    }
+    return name;
+  }
+
+  String _readTableName() {
+final name =
+        Input.withTheme(
+          theme: Theme.defaultTheme,
+          prompt: 'To which table should this column be added?',
+          defaultValue: 'interact',
+        ).interact();
+
+    if (name.isEmpty) {
+      return _readTableName();
+    }
+    return name;
+  }
+
   @override
   void execute(List<String> arguments) {
+
+
+
     if (arguments.isEmpty) {
-      stdout.writeln('  What should the migration be named?');
-      stdout.write('\x1B[1m > \x1B[0m');
-      arguments.add(stdin.readLineSync()!);
-    }
-
-    RegExp alphaRegex = RegExp(r'^[a-zA-Z][a-zA-Z0-9_/\\]*$');
-
-    if (!alphaRegex.hasMatch(arguments[0])) {
-      stdout.writeln(
-          ' \x1B[41m\x1B[37m ERROR \x1B[0m Migration must contain only letters a-z, numbers 0-9 and optional _');
-      exit(0);
-    }
+      arguments.add(_readMigrationName());
+    } 
 
     String migrationName = arguments[0].toLowerCase();
 
     if (arguments.length < 2) {
-      stdout.writeln('To which table should this column be added?');
-      stdout.writeln('\x1B[1m > \x1B[0m');
-      arguments.add(stdin.readLineSync()!);
+      arguments.add(_readTableName());
     }
 
     String tableName = Pluralize().make(arguments[1].toLowerCase());
@@ -90,8 +118,9 @@ class CreateAlterTableMigrationCommand implements Command {
     File newFile = File(filePath);
 
     if (newFile.existsSync()) {
-      stdout
-          .writeln(' \x1B[41m\x1B[37m ERROR \x1B[0m Migration already exists.');
+      stdout.writeln(
+        ' \x1B[41m\x1B[37m ERROR \x1B[0m Migration already exists.',
+      );
       exit(0);
     }
 
@@ -103,8 +132,9 @@ class CreateAlterTableMigrationCommand implements Command {
 
     newFile.writeAsString(str);
 
-    File migrate =
-        File('${Directory.current.path}/lib/database/migrations/migrate.dart');
+    File migrate = File(
+      '${Directory.current.path}/lib/database/migrations/migrate.dart',
+    );
 
     if (!migrate.existsSync()) {
       migrate.createSync(recursive: true);
@@ -113,8 +143,9 @@ class CreateAlterTableMigrationCommand implements Command {
     }
 
     final importRegExp = RegExp(r'import .+;');
-    final registryConstructorRegex =
-        RegExp(r'registry\s*\(\s*\)\s*async?\s*\{\s*([\s\S]*?)\s*\}');
+    final registryConstructorRegex = RegExp(
+      r'registry\s*\(\s*\)\s*async?\s*\{\s*([\s\S]*?)\s*\}',
+    );
 
     // Find import statement and append new import
     var importMatch = importRegExp.allMatches(migrateFileContents);
@@ -126,8 +157,9 @@ class CreateAlterTableMigrationCommand implements Command {
     }
 
     // Find registry and dropTables constructors, and replace with modified versions
-    Match? registryRepositoriesBlockMatch =
-        registryConstructorRegex.firstMatch(migrateFileContents);
+    Match? registryRepositoriesBlockMatch = registryConstructorRegex.firstMatch(
+      migrateFileContents,
+    );
 
     if (registryRepositoriesBlockMatch != null) {
       migrateFileContents = migrateFileContents.replaceAll(
@@ -140,6 +172,7 @@ class CreateAlterTableMigrationCommand implements Command {
     migrate.writeAsStringSync(migrateFileContents);
 
     stdout.writeln(
-        ' \x1B[44m\x1B[37m INFO \x1B[0m Migration [$filePath] created successfully.');
+      ' \x1B[44m\x1B[37m INFO \x1B[0m Migration [$filePath] created successfully.',
+    );
   }
 }
